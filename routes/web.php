592cc02ae1085825605de1e52e30eda2c0376e47
Toofifty <amatho250@gmail.com>
2017-11-20 01:17:46 +1100

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::prefix('api')->middleware('auth')->group(function () {
 	Route::resource('store/vaults', 'Store\VaultController');
